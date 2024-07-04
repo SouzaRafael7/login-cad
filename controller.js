c7f09df2c4d6ca4080182c2b94b671113ac6dea4
Rafael <rafael.mastellini@alunoescolassociais.org.br>
2024-07-04 12:32:38 -0300

    adicionando o dados lista com push e console para aparecer no navegador

--- controller.js
+++ controller.js
@@ -20,7 +20,8 @@ function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
 
     if(nomeUser){
-
+        dadosLista.push(nomeUser);
+        console.log(dadosLista)
     } 
     else{
         alert("Favor informar o nome");
