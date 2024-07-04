4b163995255576f91a455d9d722143f9e3b3185a
Rafael <rafael.mastellini@alunoescolassociais.org.br>
2024-07-04 07:59:04 -0300

    atualiza as funções no javascript

---
+++ controller.js
@@ -0,0 +1,13 @@
+// Validação de preenchimento
+function acessar(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
+
+    if(!loginEmail || !loginSenha){
+        alert('Favor preencher todos os campos');
+    }else{
+        alert('Campos preenchidos com sucesso');
+        
+        window.location.href = 'cadastro.html'
+    }
+}
