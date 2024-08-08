355247ce80d299a779a7f120a798b759d3f94b67
Rafael <rafael.mastellini@alunoescolassociais.org.br>
2024-08-08 12:18:12 -0300

    adicionando os botões de editar e excluir na lista e suas devidas funcionalidades

--- controller.js
+++ controller.js
@@ -33,7 +33,7 @@ function salvarUser(){
 function criarLista(){
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     for(let i = 0; i <= (dadosLista.length -1); i++){ // A propriedade length tem como responsabilidade retornar a quantidade de caracteres de uma string ou o tamanho de um array. Caso a string ou o array esteja vazio, é retornado o valor 0.
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td>  <button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button>  </td></tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>  <button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button> <button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button> </td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
 }
