7ca5200c2a3adcf417fbb247fc2a3b825dd557e1
Rafael <rafael.mastellini@alunoescolassociais.org.br>
2024-08-08 08:32:02 -0300

    adicionando as funções de excluir e editar nome dentro da lista

--- controller.js
+++ controller.js
@@ -22,6 +22,7 @@ function salvarUser(){
     if(nomeUser){
         dadosLista.push(nomeUser);
         console.log(dadosLista);
+        criarLista();
         document.getElementById('nomeUser').value = "";
     } 
     else{
@@ -31,4 +32,22 @@ function salvarUser(){
 // FUNÇÃO DE CRIAÇÃO DE LISTA
 function criarLista(){
     let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
+    for(let i = 0; i <= (dadosLista.length -1); i++){ // A propriedade length tem como responsabilidade retornar a quantidade de caracteres de uma string ou o tamanho de um array. Caso a string ou o array esteja vazio, é retornado o valor 0.
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td>  <button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button>  </td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+    }
+}
+
+// FUNÇÃO PARA EXCLUIR NOME DA LISTA
+
+function excluir(i){
+    dadosLista.splice((i - i), 1); //Splice é uma das maneiras mais úteis de manipular arrays em JavaScript. Ele permite remover ou adicionar elementos em uma posição específica de um array
+    document.getElementById('tabela').deleteRow(i);
+}
+
+//FUNÇÃO PARA EDITAR O NOME DA LISTA
+
+function editar(i){
+    document.getElementById("nomeUser").value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista[(i - 1)], 1);
 }
